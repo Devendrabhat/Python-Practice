d2abfd3698af709cd51005675f6d1c6bc64efe6f
Devendra Bhat <devendra.bhat2@gmail.com>
2017-07-30 13:04:22 +0530

    files1

---
+++ file.py
@@ -0,0 +1,8 @@
+file_name = "the_file.txt"
+
+f=open(file_name,'w')
+f.write("1. attend the m")
+f.write("2. Go home and get sleep")
+f.write("3. Make sure you had lunch")
+f.close()
+print("FIle written success")
